92734e64b4614e8321924483413df85e45dc4d33
Jimmy Li <jmyli562@gmail.com>
2023-03-27 20:32:06 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(calcTotal, 0);
+
+function calcTotal(total, num){
+    return total + num;
+}
+
